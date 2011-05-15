bcff301bc9f61e4943f76ea0c4456896efbb2eb6
Adam Duke <adam.v.duke@gmail.com>
2011-05-15 00:53:08 -0400

    initial commit

---
+++ facebook.com.js
@@ -0,0 +1,11 @@
+// delay execution until facebook finishes loading
+setTimeout(function() 
+{
+	// remove this crap
+	$("#pagelet_adbox").remove();
+	$("#pagelet_ego_pane").remove();
+	$("#pagelet_relationships").remove();
+	$("#pagelet_footer").remove();
+	$("#pagelet_friends_online").remove();
+	$(".fbDockWrapper").remove();	
+}, 300);
---
+++ github.com.js
@@ -0,0 +1,5 @@
+// swap github logo with octocat professor
+$('#header .logo img')
+  .css('width', '100px')
+  .css('margin-top', '-15px')
+  .attr('src', 'https://github.com/images/modules/training/octocat_professor.png');
